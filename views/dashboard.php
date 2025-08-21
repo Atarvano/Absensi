f681bf63c1694459a867b93fd491628fa3d9dff7
Atarvano <vanheldyirvan@gmail.com>
2025-08-21 20:35:41 +0700

    Refactor dashboard layout to use container class and improve structure

--- views/dashboard.php
+++ views/dashboard.php
@@ -10,7 +10,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary px-3">
-        <div class="container-fluid">
+        <div class="container">
             <a class="navbar-brand" href="#">Guru</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,6 +24,40 @@
             </div>
         </div>
     </nav>
+
+    <div class="container">
+        <div class="mt-5">
+            <h1>Dashboard</h1>
+            <p>Keterlambatan Siswa</p>
+        </div>
+        <div class="d-flex my-3">
+            <a href="" class="btn btn-primary">Tambah</a>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Tanggal</th>
+                    <th scope="col">Alasan Terlambat</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Irvani Heldy Fauzan</td>
+                    <td>27-10-2009</td>
+                    <td>CUci Baju</td>
+                    <td>
+                        <a href="">Update</a>
+                        <a href="">delete</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
