e7fdeb070b0cefffa3074dce258cd42560b408d8
Atarvano <vanheldyirvan@gmail.com>
2025-08-23 04:47:54 +0700

    Enhance authentication logic to include role-based redirection and sanitize password input

--- core/auth.php
+++ core/auth.php
@@ -5,18 +5,28 @@ session_start();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = htmlspecialchars($_POST['username']);
-    $password = ($_POST['password']);
+    $password = htmlspecialchars($_POST['password']);
 
-    $stmt = $pdo->prepare("SELECT * FROM guru WHERE username = :username");
+    $stmt = $pdo->prepare("
+        SELECT *, 'guru' AS role FROM `guru` WHERE username = :username
+        UNION
+        SELECT *, 'admin' AS role FROM `admin` WHERE username = :username
+    ");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        header("Location: ../views/dashboard.php");
-        exit();
+        if ($user['role'] === 'admin') {
+            $_SESSION['user_id'] = $user['id'];
+            header("Location: ../views/admin_dashboard.php");
+            exit();
+        } elseif ($user['role'] === 'guru') {
+            $_SESSION['user_id'] = $user['id'];
+            header("Location: ../views/dashboard.php");
+            exit();
+        }
 
     } else {
         $error = "Invalid username or password.";
