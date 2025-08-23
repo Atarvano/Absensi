8f4c7ad558d7f4b68ef03da7400d2fe7aa5326d6
Atarvano <vanheldyirvan@gmail.com>
2025-08-24 04:04:51 +0700

    Refactor authentication logic to set user role in session before redirecting

--- core/auth.php
+++ core/auth.php
@@ -18,11 +18,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     if ($user && password_verify($password, $user['password'])) {
+
         if ($user['role'] === 'admin') {
+            $_SESSION['role'] = $user['role'];
             $_SESSION['user_id'] = $user['id'];
             header("Location: ../views/admin_dashboard.php");
             exit();
         } elseif ($user['role'] === 'guru') {
+            $_SESSION['role'] = $user['role'];
             $_SESSION['user_id'] = $user['id'];
             header("Location: ../views/dashboard.php");
             exit();
