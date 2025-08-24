4de1828908514829551975faad90b52a874dad1e
Atarvano <vanheldyirvan@gmail.com>
2025-08-24 20:49:26 +0700

    Add header and siswa view with modal for adding student data and displaying class information

---
+++ views/siswa.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard</title>
+        <?php include '../assets/bootstrap.php'; ?>
+    </head>
+</head>
+
+<body>
+    <?php include 'components/header.php'; ?>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Kelas:</label>
+                            <select class="form-select" id="recipient-name">
+                                <option selected>Pilih Kelas</option>
+                                <option value="1">Kelas 1</option>
+                                <option value="2">Kelas 2</option>
+                                <option value="3">Kelas 3</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Nama:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="recipient-name" class="col-form-label">Tanggal:</label>
+                            <input type="date" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="message-text" class="col-form-label">Alasan Terlambat:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Send message</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="mt-5">
+            <h1>Dashboard</h1>
+            <p>Siswa</p>
+        </div>
+        <div class="d-flex my-3">
+            <a href="" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal">Tambah</a>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Kelas</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once '../model/siswa.php';
+                foreach ($siswa as $row) {
+                    ?>
+                    <tr>
+                        <th scope="row"></th>
+                        <td><?= $row['nama']; ?></td>
+                        <td><?= $row['nama_kelas']; ?></td>
+                        <td>
+                            <a href="update.php?id=<?= htmlspecialchars($row['id']); ?>">Update</a>
+                            <a href="delete.php?id=<?= htmlspecialchars($row['id']); ?>">Delete</a>
+                        </td>
+
+                    <?php } ?>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
