7b5957591dbabce3d42f8f8327c0e9393ad1824b
Atarvano <vanheldyirvan@gmail.com>
2025-08-25 20:20:03 +0700

    Add .gitignore file and update header to display user role; conditionally show navigation links for admin

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
+
 if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
+    header('Location: views/login.php');
     exit();
 }
 
-
-
 if (isset($_SESSION['login'])) {
     header('Location: views/dashboard.php');
-} else {
-    header('Location: views/login.php');
+    exit();
 }
-
-
 ?>
--- views/components/header.php
+++ views/components/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary px-3">
     <div class="container">
-        <a class="navbar-brand" href="#">Guru</a>
+        <a class="navbar-brand" href="#"><?php echo $_SESSION['role'] === 'admin' ? 'Admin' : 'Guru'; ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,8 +8,10 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
                 <a class="nav-link" aria-current="page" href="dashboard.php">Home</a>
-                <a class="nav-link" aria-current="page" href="siswa.php">Siswa</a>
-                <a class="nav-link" aria-current="page" href="kelas.php">Kelas</a>
+                <?php if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'): ?>
+                    <a class="nav-link" aria-current="page" href="siswa.php">Siswa</a>
+                    <a class="nav-link" aria-current="page" href="kelas.php">Kelas</a>
+                <?php endif; ?>
                 <a class="nav-link" href="../core/logout.php">Logout</a>
             </div>
         </div>
